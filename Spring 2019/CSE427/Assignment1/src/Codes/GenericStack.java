f7f6d1ca86a6a20b930b8ff51147875c3be83466
Sohanur-Rahman642 <rahman.sohanur@northsouth.edu>
2019-03-20 19:41:35 +0600

    Added a test method for a normal push

--- Spring 2019/CSE427/Assignment1/src/Codes/GenericStack.java
+++ Spring 2019/CSE427/Assignment1/src/Codes/GenericStack.java
@@ -7,5 +7,7 @@ public class GenericStack<Object> {
 	}
 	
 	
+	
+	
 
 }
--- Spring 2019/CSE427/Assignment1/src/Codes/GenericStackTest.java
+++ Spring 2019/CSE427/Assignment1/src/Codes/GenericStackTest.java
@@ -13,11 +13,36 @@ public class GenericStackTest {
 	/*
 	 * @Test will get a compilation error due to missing of getSize method in the GenericStack Class
 	 * That's what we wanted before using Refactoring 
+	 * After adding getSize() it will pass the test
 	 */
 	public void sizeOfStackShouldBeZero() {
 		GenericStack<Object> gs = new GenericStack<>();
 		assertThat("New Stack Should be Empty", gs.getSize(),is (0));
 		
+	}
+	
+	@Test
+	/*
+	 * Test the codes of push 
+	 * Implement the same code in the GenericStack class
+	 */
+	public void testANormalPush() {
+		Object actual = push(10);
+		assertEquals(10,actual);
+		
+		
+	}
+	
+	public Object push(Object x) {
+		int top=0;
+			Object[] elements = null;
+		
+		    GenericStack<Object> gs = new GenericStack<>();
+		    if(top<gs.getSize()) {
+			elements[top] = x;
+			top++;	
+		}
+		    return x;
 	}
 
 }
