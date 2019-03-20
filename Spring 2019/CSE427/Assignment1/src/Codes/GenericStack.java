8527699a0dcf0d35f52036289d9abf6fca53948d
Sohanur-Rahman642 <rahman.sohanur@northsouth.edu>
2019-03-20 19:45:23 +0600

    # Implemented the push method after being tested from the GenericStack class

--- Spring 2019/CSE427/Assignment1/src/Codes/GenericStack.java
+++ Spring 2019/CSE427/Assignment1/src/Codes/GenericStack.java
@@ -2,9 +2,24 @@ package Codes;
 
 public class GenericStack<Object> {
 	
+	private Object elements[];
+	private int size;
+	private int top;
+	
 	public int getSize() {
 		return 0 ;
 	}
+	
+	public void push (Object x) {
+		if(top<size) {
+			elements[top] = x;
+			top++;
+			
+		}
+	}
+	
+
+	
 	
 	
 	
